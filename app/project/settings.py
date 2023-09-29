6dd9f7d0657c72cabf9f282a23bdeb01d81f12b1
Sérgio Dantas <sergiodnts828@gmail.com>
2023-09-29 06:52:34 -0300

    configure .env vars and settings.py

--- app/project/settings.py
+++ app/project/settings.py
@@ -10,22 +10,26 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from os import getenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = BASE_DIR.parent / 'data' / 'web'
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&g#_@1pxud_@&5)_m49#mdaolm&-p0n+&t)m*anr-(3e)w7hzv'
+SECRET_KEY = getenv('SECRET_KEY', 'CHANGE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(getenv('DEBUG', 0)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    host for host in getenv('ALLOWED_HOSTS').split(',')
+]
 
 
 # Application definition
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': getenv('DB_ENGINE', 'CHANGE'),
+        'NAME': getenv('POSTGRES_DB', 'CHANGE'),
+        'USER': getenv('POSTGRES_USER', 'CHANGE'),
+        'PASSWORD': getenv('POSTGRES_PASSWORD', 'CHANGE'),
+        'HOST': getenv('POSTGRES_HOST', 'CHANGE'),
+        'PORT': getenv('POSTGRES_PORT', 'CHANGE'),
     }
 }
 
@@ -103,9 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Recife'
 
 USE_I18N = True
 
@@ -116,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = DATA_DIR / 'static'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = DATA_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
