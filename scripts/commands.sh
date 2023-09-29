48ee416a9b1d7747280c3620f5d7c6a32ce5f581
Sérgio Dantas <sergiodnts828@gmail.com>
2023-09-29 08:03:37 -0300

    create docker-compose.yml and fix postgres host var

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -9,7 +9,7 @@ done
 
 echo "✅ Postgres database started succesfully ($POSTGRES_HOST $POSTGRES_PORT)."
 
-python manage.py collectstatic
-python manage.py makemigrations
-python manage.py migrate
-python manage.py runserver
+python manage.py collectstatic --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py runserver 0.0.0.0:8000
