3877c50f99ba2b339266ab4307494a74153d56f2
Sérgio Dantas <sergiodnts828@gmail.com>
2023-10-05 21:29:07 -0300

    turn MenuLinkAdmin inline and relate (fk) to SetupAdmin

--- app/setup/admin.py
+++ app/setup/admin.py
@@ -1,20 +1,18 @@
 from django.contrib import admin
-from django.http.request import HttpRequest
 
 from setup.models import MenuLink, Setup
 
 
-@admin.register(MenuLink)
-class MenuLinkAdmin(admin.ModelAdmin):
-    list_display = 'id', 'text', 'url_or_path'
-    list_display_links = 'id', 'text', 'url_or_path'
-    search_fields = 'id', 'text', 'url_or_path'
+class MenuLinkInline(admin.TabularInline):
+    model = MenuLink
+    extra = 1
 
 
 @admin.register(Setup)
 class SetupAdmin(admin.ModelAdmin):
-    list_display = 'title', 'description'
-    list_display_link = 'title', 'description'
+    list_display = 'title', 'description',
+    list_display_link = 'title', 'description',
+    inlines = MenuLinkInline,
 
     @staticmethod
     def has_add_permission(request) -> bool:
--- app/setup/models.py
+++ app/setup/models.py
@@ -1,19 +1,6 @@
 from django.db import models
 
 
-class MenuLink(models.Model):
-    class Meta:
-        verbose_name = 'Menu Link'
-        verbose_name_plural = 'Menu Links'
-    
-    text = models.CharField(max_length=50)
-    url_or_path = models.CharField(max_length=2048)
-    new_tab = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.text
-
-
 class Setup(models.Model):
     class Meta:
         verbose_name = 'Setup'
@@ -31,3 +18,19 @@ class Setup(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class MenuLink(models.Model):
+    class Meta:
+        verbose_name = 'Menu Link'
+        verbose_name_plural = 'Menu Links'
+    
+    text = models.CharField(max_length=50)
+    url_or_path = models.CharField(max_length=2048)
+    new_tab = models.BooleanField(default=False)
+    setup = models.ForeignKey(
+        'Setup', on_delete=models.CASCADE, blank=True, null=True, default=None
+    )
+
+    def __str__(self):
+        return self.text
