e3c722e1b7f43640b01c2947bac3a338d86be01e
Sérgio Dantas <sergiodnts828@gmail.com>
2023-10-12 10:46:08 -0300

    add favicon field in setup model

--- app/setup/models.py
+++ app/setup/models.py
@@ -5,7 +5,7 @@ class Setup(models.Model):
     class Meta:
         verbose_name = 'Setup'
         verbose_name_plural = 'Setup'
-    
+
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=255)
 
@@ -16,6 +16,11 @@ class Setup(models.Model):
     show_pagination = models.BooleanField(default=True)
     show_footer = models.BooleanField(default=True)
 
+    favicon = models.ImageField(
+        upload_to='assets/favicon/%Y/%m/',
+        blank=True, default=''
+    )
+
     def __str__(self):
         return self.title
 
@@ -24,12 +29,13 @@ class MenuLink(models.Model):
     class Meta:
         verbose_name = 'Menu Link'
         verbose_name_plural = 'Menu Links'
-    
+
     text = models.CharField(max_length=50)
     url_or_path = models.CharField(max_length=2048)
     new_tab = models.BooleanField(default=False)
     setup = models.ForeignKey(
-        'Setup', on_delete=models.CASCADE, blank=True, null=True, default=None
+        'Setup', on_delete=models.CASCADE,
+        blank=True, null=True, default=None
     )
 
     def __str__(self):
